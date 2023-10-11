6d8dab7b266128c1a46a2be4b254d4af104d535a
时天博 <12055593+jinwanliegetongkuai@user.noreply.gitee.com>
2023-10-11 14:29:07 +0800

    '提交格式化代码'

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,18 @@
 </template>
 
 <script setup lang="ts">
-
+const str = '我爱你祖国'
+console.log(str)
 </script>
 
 <style lang="scss" scoped>
-
+.box {
+  width: 600px;
+  height: 400px;
+  background: #000;
+  h1 {
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
