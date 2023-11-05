d8efac00a4d1e88ea3e5193ba3c49809a47cf24b
时天博 <12055593+jinwanliegetongkuai@user.noreply.gitee.com>
2023-11-05 11:32:13 +0800

    11

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,6 @@
+// 用于配置代码提交的规范性检查工具 commitlint.
+// commitlint 是一个用于验证代码提交信息格式的工具，
+// 它可以确保提交信息遵循一定的约定，便于团队成员理解和管理代码提交历史。
 module.exports = {
     ignores: [(commit) => commit.includes('init')],
     extends: ['@commitlint/config-conventional'],
